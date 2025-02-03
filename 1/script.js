1b8497399c4a1fb3fd3436e3da30d99ce2821d89
philcheen <philcheen@outlook.com>
2025-02-03 09:33:56 -0500

    react update

--- 1/script.js
+++
@@ -1,5 +0,0 @@
-function onShowSideBar(){
-    let btn = document.getElementById("iconToggle");
-    let sideBar=document.getElementById("sidemenu_webview");
-    sideBar.style.display=btn.checked?"block":"none";
-}

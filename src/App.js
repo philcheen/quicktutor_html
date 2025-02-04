270881cddfa1d174fa2a5d1717f911cb942a497d
philcheen <philcheen@outlook.com>
2025-02-04 05:09:45 -0500

    autostart

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
     return (
         <>
             {
-                [0,1,2].map(function (i) {
-                    return(
+                [0, 1, 2].map(function (i) {
+                    return (
                         <React.Fragment key={i}>
                             Counter {i} defined by {`<Counter init='5' end='${10 + i}' />`} : <br/>
-                            <Counter init='5' end={10 + i}/>
+                            <Counter init='5' end={10 + i} autostart={i > 1 ? 1 : 0}/>
                             <br/><br/>
                         </React.Fragment>
                     )
--- src/Counter.js
+++ src/Counter.js
@@ -1,27 +1,54 @@
 import React, {useState, useEffect} from 'react';
 
-function Counter({init, end}) {
+function Counter({init, end, autostart}) {
     init = parseInt(init || 0);
     end = parseInt(end || 0);
+    autostart = parseInt(autostart || 0);
+
     const [count, setCount] = useState(init);
+    const [start, setStart] = useState(true);
+
+    function increase() {
+        setCount((count) => count + 1);
+    };
     useEffect(function () {
-        if (end && count >= end) return;
-        let timer = setInterval(function () {
-            setCount((count) => count + 1);
-            console.log("count=", count);
-        }, 1000);
-        return function () {
-            clearInterval(timer);
+        if (autostart) restart();
+    },autostart);
+    useEffect(function () {
+        if (!start) {
+            let timer = setInterval(function () {
+                setCount((count) => {
+                    let newCount = count + 1;
+                    if (newCount >= end) setStart(true);
+                    return newCount;
+                });
+            }, 1000);
+            return function () {
+                clearInterval(timer);
+            }
         }
     });
+
+
+    function restart() {
+        setStart(false);
+        setCount(init);
+    }
     return (
         <>
             Initial value of the counter is: {init}
             <br/>
+            End of the counter at: {end}
+            <br/>
             The counter is: {count}
+            <br/>
             {
-                (end && count >= end) ? <b style={{color:"red", fontsize:"20px"}}>Counter stopped</b>
-                    : <i>Counter in progress</i>
+                (start) ?
+                    <>
+                        <b>Counter stopped</b>&nbsp;
+                        <button onClick={restart}>Start</button>
+                    </> :
+                    <i>Counter in progress</i>
             }
         </>
     );

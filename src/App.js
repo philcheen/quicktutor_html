8ae8d5e1c769eba62d513081682f6559809d0a24
philcheen <philcheen@outlook.com>
2025-02-04 03:33:24 -0500

    use Array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,7 +6,19 @@ import Counter from './Counter';
 
 function App() {
     return (
-        <Counter init={10}/>
+        <>
+            {
+                [0,1,2].map(function (i) {
+                    return(
+                        <React.Fragment key={i}>
+                            Counter {i} defined by {`<Counter init='5' end='${10 + i}' />`} : <br/>
+                            <Counter init='5' end={10 + i}/>
+                            <br/><br/>
+                        </React.Fragment>
+                    )
+                })
+            }
+        </>
     );
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
 
-function Counter(props) {
-    const init = props.init;
+function Counter({init, end}) {
+    init = parseInt(init || 0);
+    end = parseInt(end || 0);
     const [count, setCount] = useState(init);
     useEffect(function () {
+        if (end && count >= end) return;
         let timer = setInterval(function () {
-            setCount((count)=>count + 1);
+            setCount((count) => count + 1);
             console.log("count=", count);
         }, 1000);
         return function () {
@@ -15,8 +17,11 @@ function Counter(props) {
     return (
         <>
             Initial value of the counter is: {init}
-            <br />
+            <br/>
             The counter is: {count}
+            {
+                (end && count >= end) ? <b>Counter stopped</b> : <i>Counter in progress</i>
+            }
         </>
     );
 }

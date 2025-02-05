22cf3ce3e05d91a50112907d54aa5d2f93f8884c
philcheen <philcheen@outlook.com>
2025-02-04 21:29:17 -0500

    promise Ex

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import { useState } from "react";
+import {useState} from "react";
 import Items from "./Items";
+
 function App() {
+
+    function loadImage(url) {
+        return new Promise(function (resolve, reject) {
+            const img = new Image();
+            img.onload = function () {
+                resolve(img);
+            };
+            img.onerror = function () {
+                reject("Unable to load the image.");
+            };
+            img.src = url;
+        });
+    }
+
+    loadImage("file:///D:/Work/Example/QuickTutor_files/a-tutor-fit.svg")
+        .then(function (img) {
+            document.body.appendChild(img);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+
     const [items, setItems] = useState([]);
+
     function add() {
         let item = "Item " + (items.length + 1);
         items.push(item);
         setItems([...items]);
     }
+
     return (
         <>
             <button onClick={add}>Add Item</button>
@@ -17,4 +42,5 @@ function App() {
         </>
     );
 }
+
 export default App;
